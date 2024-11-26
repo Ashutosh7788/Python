f0cfa9b9279bc0066ff90548095373c4400174ac
Ashutosh Behera <151955640+Ashutosh7788@users.noreply.github.com>
2024-11-26 20:34:08 +0530

    Add files via upload

---
+++ 01_Pratice set/Problem03.py
@@ -0,0 +1,5 @@
+# external module use
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hey Ashutosh Behera  What are you doing. When did we go to take our dinner ")
+engine.runAndWait()
---
+++ 01_Pratice set/Problem04.py
@@ -0,0 +1,8 @@
+import os
+# select the directory whose content you want to the list
+directory_path = '/ New folder'
+# use the os module to lis the directory content
+content = os.listdir(directory_path)
+#print the content of the directory
+print(content)
+      
