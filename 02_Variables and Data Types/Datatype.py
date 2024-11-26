d23a7128a9802766ec04950270df4ab62cb0327b
Ashutosh Behera <151955640+Ashutosh7788@users.noreply.github.com>
2024-11-26 22:53:32 +0530

    Add files via upload

---
+++ 02_Variables and Data Types/Datatype.py
@@ -0,0 +1,7 @@
+a = 1 #a is  an integr
+b = 4.8 #b is a floating point number
+c = "ASHUTOSH" #c is a stirng 
+d = False #d is a boolean variable
+e = None #e is a none type variable
+# @param = 34 #Invlaid  due to @ symbol 
+# p@ram # Invalid due to  @ symbol
