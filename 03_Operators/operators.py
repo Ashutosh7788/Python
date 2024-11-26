b702f0ddca37f8752d6dc0cc859f9c3894e071bb
Ashutosh Behera <151955640+Ashutosh7788@users.noreply.github.com>
2024-11-26 22:54:02 +0530

    Add files via upload

---
+++ 03_Operators/operators.py
@@ -0,0 +1,38 @@
+# Arithmetic Operators 
+a = 43
+b = 4 
+c = a+b
+print(c)
+
+# Assignment Operators 
+a = 4-2 # Assign 4-2 in a 
+print(a)
+b = 6 
+b += 3 #Increment the value of b by 3 and then assign it to b
+b -= 3 # Decrement the value of b by 3 and then assign it to  b 
+print(b) 
+
+# Comparison operatoer
+d = 5>4
+r = 5!=5
+print(d,r)
+
+ # Logical operators
+e = True or False
+# Truth table of 'OR'
+
+print("True or False is", True or False)
+print("True or True is", True or True)
+print("False or True is", False or True)
+print("False or False is", False or False)
+
+# Truth table of 'AND'
+
+print("True and False is", True  and False)
+print("True  and True is", True  and True)
+print("False and True is", False  and True)
+print("False  and False is", False  and False)
+
+# Truth table of 'NOT'
+
+print(not(True and False))
