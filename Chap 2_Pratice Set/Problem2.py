4d8d82c8ba1c7d4c294e799abba7511c5c34916e
Ashutosh Behera <151955640+Ashutosh7788@users.noreply.github.com>
2024-11-27 22:53:13 +0530

    Adding pratice questions

---
+++ Chap 2_Pratice Set/Problem2.py
@@ -0,0 +1,3 @@
+a=52
+b=3
+print("Reminder whem a is divided by b is", a%b)
---
+++ Chap 2_Pratice Set/Problem5.py
@@ -0,0 +1,3 @@
+a=int(input("Enter thr number 1:"))
+b=int(input("Enter thr number 2:"))
+print("The average of these two number is", (a+b)/2)
