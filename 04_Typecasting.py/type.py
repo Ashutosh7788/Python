c69ae3b48281b6a2ffac31635215a4863f9d5e1b
Ashutosh Behera <151955640+Ashutosh7788@users.noreply.github.com>
2024-11-27 22:29:57 +0530

    Add files via upload

---
+++ 04_Typecasting.py/type.py
@@ -0,0 +1,9 @@
+a = "31.3"
+b = float(a)  #  A BUT THE TYPE SHOULD BE FLOAT
+t = type(b)
+print(t)
+# TYPE CASTING 
+# There are many function to coveret one data type into another.
+# str(31)="31" #integer to string conversion 
+# int("32")= "32" # string  to intger  conversion
+# float (32)=32.0 #integer to float converin 
