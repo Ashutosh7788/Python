944808af595e707aa530711289f0704bec5983e7
Ashutosh Behera <151955640+Ashutosh7788@users.noreply.github.com>
2024-12-02 20:20:58 +0530

    Add files via upload

---
+++ Chap4_Pratice set/Problem03.py
@@ -0,0 +1,3 @@
+# Check that a type cannot be changed in python
+t=(58,85,"AShu")
+t[2]="AShutoh"
---
+++ Chap4_Pratice set/Problem04.py
@@ -0,0 +1,4 @@
+# Write a program to sum a list  with 4 numbers.
+a =[234,343,434,3535]
+
+print(sum(a))
